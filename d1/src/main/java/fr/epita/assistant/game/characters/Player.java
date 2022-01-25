b4610258e773dc446eac80c4901c2f5eba426d4b
Alexandre Gautier <alexandre.gautier@epita.fr>
2022-01-25 13:22:25 +0100

    fix

--- d1/src/main/java/fr/epita/assistant/game/characters/Player.java
+++ d1/src/main/java/fr/epita/assistant/game/characters/Player.java
@@ -114,9 +114,9 @@ public final class Player extends Character implements Attacker {
     {
         switch (direction)
         {
-            case UP -> this.coord.setX(this.coord.getX() - 1);
+            case UP -> this.coord.setX(this.coord.getY() - 1);
             case DOWN -> this.coord.setX(this.coord.getY() + 1);
-            case LEFT -> this.coord.setY(this.coord.getY() - 1);
+            case LEFT -> this.coord.setY(this.coord.getX() - 1);
             default -> this.coord.setY(this.coord.getX() + 1);
         }
     }
